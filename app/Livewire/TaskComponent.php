8b5c4f12091cc81ff1a2671794870ff2aebe7e8e
Andres Felipe Mejia <amejia@cidenet.com.co>
2024-07-18 10:39:44 -0500

    Se crea procedimientos para compartir tareas con otros usuarios, livewire

--- app/Livewire/TaskComponent.php
+++ app/Livewire/TaskComponent.php
@@ -3,24 +3,37 @@
 namespace App\Livewire;
 
 use App\Models\Task;
+use App\Models\User;
 use Livewire\Component;
 
 class TaskComponent extends Component
 {
     public $tasks = [];
+    public $users = [];
     public $id;
     public $title;
     public $description;
+    public $user_id;
+    public $permission;
     public $modal = false;
+    public $modalShared = false;
 
     public function mount()
     {
         $this->geTasks();
     }
 
+    public function loadTasksAll(){
+        $this->geTasks();
+    }
+
     public function geTasks()
     {
-        $this->tasks = auth()->user()->tasks;
+        $user = auth()->user();
+        $myTasks = auth()->user()->tasks;
+        $tasksShared = $user->sharedTask;
+        $this->tasks = $myTasks->merge($tasksShared)->sortByDesc('id');
+        $this->users = User::where('id', '!=', $user->id)->get();
     }
 
     public function render()
@@ -40,19 +53,48 @@ class TaskComponent extends Component
         $this->modal = !$this->modal;
     }
 
+    public function toggleModalShared(Task $task)
+    {
+        $this->id = $task->id;        
+        $this->modalShared = !$this->modalShared;
+    }
+
     public function addOrUpdateTask()
     {
-        Task::updateOrCreate([
-            'id' => $this->id
-        ], [
-            'title' => $this->title,
-            'description' => $this->description,
-            'status' => 'new',
-            'user_id' =>  auth()->user()->id
-        ]);
+        if ($this->id) {
+            $task = Task::find($this->id);
+            $task->update([
+                'title' => $this->title,
+                'description' => $this->description,
+            ]);
+        } else {
+            $task = Task::create([
+                'title' => $this->title,
+                'description' => $this->description,
+                'status' => 'new',
+                'user_id' => auth()->user()->id
+            ]);
+        }
 
         $this->geTasks();
         $this->toggleModal();
+    }
+
+    public function sharedTask()
+    {     
+           
+        $task = Task::find($this->id);
+        $user = User::find($this->user_id);
+        $user->sharedTask()->attach($task->id, ['permission' => $this->permission]);        
+        $this->geTasks();
+        $this->modalShared = false;
+    }
+
+    public function unSharedTask(Task $task)
+    {
+        $user = User::find(auth()->user()->id);
+        $user->sharedTask()->detach($task->id);
+        $this->geTasks();
     }
 
     public function updateTask(Task $task)
